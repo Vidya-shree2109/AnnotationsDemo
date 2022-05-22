7440c9efde32d758e1a0ac5a70772084376298e3
Vidyashree N M <vidyashree2109@gmail.com>
2022-05-23 01:34:12 +0530

    [Vidya] Add : Demo Program Of Annotations Is Done

---
+++ AnnotationsDemo/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnotationsDemo
+{
+    public class Employee
+    {
+        
+        [Required(ErrorMessage = "Employee {0} is required")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name should be minium 3 characters and of 100 characters")]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+        [Range(18, 99, ErrorMessage = "Age should be above 18")]
+        public int Age { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
---
+++ AnnotationsDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnnotationsDemo;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("\t\t\t\t\tVALIDATION OF EMPLOYEE CLASS\t\t\t\t\t");
+        Console.WriteLine("\t\t\t\t\t----------------------------\t\t\t\t\t\n");
+
+        Employee employee = new Employee();
+
+        employee.Name = "XYZ";
+        employee.Age = 20;
+        employee.PhoneNumber = "4567853";
+        employee.Email = "xyz@gmail.com";
+        ValidationContext context = new ValidationContext(employee, null, null);
+        List<ValidationResult> results = new List<ValidationResult>();
+        bool valid = Validator.TryValidateObject(employee, context, results, true);
+        if (!valid)
+        {
+            foreach (ValidationResult Totalresult in results)
+            {
+                Console.WriteLine("Member Name: {0}", Totalresult.MemberNames.First(), Environment.NewLine);
+                Console.WriteLine("Error Msg : {0}{1}", Totalresult.ErrorMessage, Environment.NewLine);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Name : " + employee.Name + "\n" + "Age : " + employee.Age + "\n" + "Phone Number : " + employee.PhoneNumber + "\n" + "Email : " + employee.Email);
+        }
+    }
+}
